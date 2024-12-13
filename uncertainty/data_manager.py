57cb4dca372df39c23eb404e035af1b5e2d0b8ab
Simon Marwitz <simon.jakob.marwitz@uni-weimar.de>
2024-12-14 00:00:02 +0100

    daily crontab backup Sat Dec 14 00:00:02 CET 2024

--- uncertainty/data_manager.py
+++ uncertainty/data_manager.py
@@ -680,9 +680,14 @@ class DataManager(object):
             jid_inds = np.arange(chunks_submit)
             np.random.shuffle(jid_inds)
             
-            m_lags_steps = [50,150,250,350,450,550,650,750,850,950,1001]
-            factor = 0.035
-            tasks = [setup_eval.options(memory=num*factor*1024*1024*1024, name= f'{num*factor} GB {self.title}') for num in m_lags_steps]
+            # m_lags_steps = [50,150,250,350,450,550,650,750,850,950,1001]
+            m_lags_steps = [245, 285, 330, 365, 430, 485, 575, 625, 725, 800, 900, 1001]
+
+            # factor = 0.05
+            # tasks = [setup_eval.options(memory=num*factor*1024*1024*1024, name= f'{num*factor} GB {self.title}') for num in m_lags_steps]
+            factors = (0.0000875, -0.01125, 2.3475)
+            tasks = [setup_eval.options(memory=(num**2*factors[0]+num*factors[1] + factors[2])*1024*1024*1024, name= f'{(num**2*factors[0]+num*factors[1] + factors[2])} GB {self.title}') for num in m_lags_steps]
+            
             
             for jid_ind in jid_inds:
                 jid=in_ds.ids[jid_ind].item()
